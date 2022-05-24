ccdb35a7e8fb9ee90368b104700ffe3b93ecd2d6
zhouzhiwengang <zhouzhiwengang@163.com>
2022-05-25 01:29:49 +0800

    redis 通用功能封装

--- SmartUrban/SmartUrban-Consumer/src/main/java/com/zzg/controller/UserController.java
+++ SmartUrban/SmartUrban-Consumer/src/main/java/com/zzg/controller/UserController.java
@@ -2,7 +2,9 @@ package com.zzg.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.zzg.model.SysUser;
+import com.zzg.redis.inter.impl.RedisCacheUtil;
 import com.zzg.service.SysUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +14,15 @@ public class UserController {
     @Reference
     private SysUserService sysUserService;
 
+    @Autowired
+    private RedisCacheUtil redisCacheUtil;
+
     @RequestMapping("/hello")
     public String get() {
         SysUser obj = sysUserService.selectById(1);
         System.err.println(obj.getId());
+        boolean target = redisCacheUtil.set(String.valueOf(obj.getId()), obj);
+        System.out.println("是否添加成功:" + target);
         return "Hello Spring Boot!";
     }
 }
